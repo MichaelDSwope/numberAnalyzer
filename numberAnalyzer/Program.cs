8e1f34314e9a9aed115f53d751fc1df47bbbac62
MichaelDSwopeVox <digitalwhiterabbithole@gmail.com>
2023-07-17 02:10:27 -0400

    First numberAnalyzer Commit

--- numberAnalyzer/Program.cs
+++ numberAnalyzer/Program.cs
@@ -1,77 +1,67 @@
 ﻿//number ananyzer and decision maker
 using System;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using System.Text;
 
 
 namespace numberAnalyzer;
 
+//bool should continue
 
-
-class program
+class program 
 {
     static void Main(string[] args)
     {
+        int i;
+      
+        int oddNum;
+        int evenNum;
+        string y = "yes";
+
         Console.WriteLine("Enter your name.: ");
-        var userName = Console.ReadLine();
-        Console.WriteLine("Hello there," + userName);
+        string userName = Console.ReadLine();
+      
+        Console.WriteLine(" \n Hello " + userName + ", Please Enter a number. :");
+
+        bool yes = true;
 
 
-
-
-        var i = 0;
-        Console.WriteLine("Enter a number between 1 and 100.: ");
-        
-        string numPick = Console.ReadLine();
-
+        i = int.Parse(Console.ReadLine());
         if (i % 2 == 0)
         {
-            Console.WriteLine("its an enen number");
+            Console.Write("Entered Number is an Even Number");
+            Console.Read();
         }
         else
         {
-            Console.WriteLine("its an even number");
-
-            for (i = 1; i <= 100; i++)//show numbers 1-100
-                if (i % 2 == 0) ;//even numbers
-
-
-
-            if (i <= 60) ; //A.
-            Console.WriteLine("Odd and less than 60." + userName);
-
-            for (i = 2; i <= 24; i++) ;  //B.   
-            Console.WriteLine("Even and less than 25." + userName);
-
-            //if (i >= 26; i => 60;  //C.          
-            //Console.WriteLine("Even and between 26 and 60 inclusive." + userName);
-
-            if (i > 60) ;   //D.
-            Console.WriteLine("Even and greater than 60." + userName);
-
-            if (i > 60) ;   //E.
-            Console.WriteLine("Odd and greater than 60." + userName);
-
-
-        if (i % 2 != 0) ;//Odd numbers
-
-            //*******exit program thread*****************
-        Console.WriteLine("Press any key to exit");
-        Console.ReadKey();
+            Console.Write("Entered Number is an Odd Number");
+            Console.Read();
+        }
+        if (i % 2 != 0 && i <60)//a.1 point odd less than 60
+        {
+            Console.WriteLine( "Odd and less than 60" );
+        }
+        if (i % 2 == 0 && i >= 2 && i < 24)//b.1 point even and less than 25, 2-24 inc
+        {  
+            Console.WriteLine("Even and less than 25."); 
+        }
+        if (i % 2 == 0 && i >= 26 && i <= 60)//c.1 point even between 26 and 60 inclusive.
+        {
+            Console.WriteLine("Even and between 26 and 60 inclusive.");
+        }
+        if (i % 2 == 0 && i > 60) 
+        {
+            Console.WriteLine("Even and greater than 60.");//d.even and greater than 60.
+        }
+        if (i % 2 != 0 && i > 60) 
+        {
+            Console.WriteLine("Odd and greater than 60.");//e.Odd and greater than 60.
         }
 
     }
-
 }
-        
-        
-
-    
-
-
-
-
-
 
 
 
